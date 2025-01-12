f68f165d3807b382a300258047b6ace713b79376
Satina213 <will.mcminn97@gmail.com>
2025-01-12 10:03:14 -0600

    updated form with backend verification

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django import forms
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 tasks = ["foo", "bar", "baz"]
 # Create your views here.
@@ -9,4 +14,15 @@ def index(request):
     })
 
 def add(request):
-    return render(request, "tasks/add.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
